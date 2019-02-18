f2d26a3b00657c781eba3014a07a6f8fe7f116bf
Rajesh Kumar Sahoo <rajesh2012acs@gmail.com>
2019-02-18 21:17:13 +0530

    Updated Code
    Updated Code for code renduncay

--- logRepository.py
+++ logRepository.py
@@ -45,83 +45,38 @@ class logRepository():
 
 
     def dbInsert(self,loggerInfo):
-
         try:
-
             cLogInfo=loggerInfo
-            BOOK={}
+            configLogDB={}
+            ts=float(cLogInfo['created'])
+            timeStamp=datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S,%f')[:-3]
+            print("\r\n")
+            auditMsg=str(timeStamp)+', '+cLogInfo['levelname']+', '+cLogInfo['msg']
+            logMsges=cLogInfo["msg"]
+            logMsges=logMsges.split(',')
+            botID=logMsges[0]
+            botInstanceID=logMsges[1]
 
-            try:
-                
+            #print(logMsges[0:2])
+            #print("\r\n")
+            messageLog=','.join(logMsges[2:])
+            configLogDB = self.msgLogger(timeStamp,cLogInfo['levelname'],botID,botInstanceID,messageLog)
+            print(configLogDB)
+            #print("\r\n")
+            #print(auditMsg)
+            #BOOK["auditMsg"]=auditMsg
+            #audit.insert(BOOK)
 
-                if "audit" in cLogInfo['funcName']:
-                    ts=float(cLogInfo['created'])
-                    timeStamp12=datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S,%f')[:-3]
-                    print("\r\n")
-                    auditMsg=str(timeStamp12)+', '+cLogInfo['levelname']+', '+cLogInfo['msg']
-                    logMsges=cLogInfo["msg"]
-                    logMsges=logMsges.split(',')
-                    #print(logMsges[0:2])
-                    #print("\r\n")
-                    messageLog=','.join(logMsges[2:])
-                    configLogDB_Audit = self.msgLogger(timeStamp12,cLogInfo['levelname'],logMsges[0],logMsges[1],messageLog)
-                    print(configLogDB_Audit)
-                    print("\r\n")
-                    print(auditMsg)
-                    BOOK["auditMsg"]=auditMsg
-                    #audit.insert(BOOK)
-                    self.audit.insert(configLogDB_Audit)#BOOK)
-                    print("\r\n")
-
-            except Exception as exc:
-                print ("Audit Log insertion failure")
-                print (exc)
-    ##############################################################################################################
-
-
-    #######For Debug report below is to create the message########################################################
-
-            try:
-                if "dev" in cLogInfo['funcName']:
-                    print("hi Debug")
-                    print(cLogInfo['funcName'])
-                    ts1=float(cLogInfo['created'])
-                    print(ts1)
-                    timeStamp21=datetime.utcfromtimestamp(ts1).strftime('%Y-%m-%d %H:%M:%S,%f')[:-3]
-                    print(timeStamp21)
-                    print("\r\n")
-                    devMsg=str(timeStamp21)+', '+cLogInfo['levelname']+', '+cLogInfo['msg']
-
-                    ##############*************##############
-                    devlogMsges=cLogInfo["msg"]
-                    devlogMsges=devlogMsges.split(',')
-                    print(devlogMsges[0:2])
-                    #print("\r\n")
-                    devMessageLog=','.join(devlogMsges[2:])
-                    configLogDB_Debug = self.msgLogger(timeStamp21,cLogInfo['levelname'],devlogMsges[0],devlogMsges[1],devMessageLog)
-                    print(configLogDB_Debug)
-                    ##############************###############
-                    print(devMsg)
-                    BOOK["debugMsg"]=devMsg
-                    #debug.insert(BOOK)
-                    self.debug.insert(configLogDB_Debug)
-                    print("\r\n")
-
-
-            except Exception as exc:
-                print ("Debug Log insertion failure")
-                print (exc)
-            # try:
-            #     self.connection.close()
-                
-            # except Exception as exc:
-            #     print("Connection couldn't be closed",exc)
+            if "audit" in cLogInfo['funcName']:
+                self.audit.insert(configLogDB)#BOOK)
+                print("\r\n")
+            elif "dev" in cLogInfo['funcName']:
+                self.debug.insert(configLogDB)
 
         except Exception as exc:
             print ("Log insertion failure")
             print (exc)
 
         
-
 
         
